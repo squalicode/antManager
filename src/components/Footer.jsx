86fd2c0a78b8b24120e94452f3344f38aac76e9d
Jude <judeserena@outlook.com>
2023-03-28 01:43:27 +0200

    1.0 sharing is caring

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
       <footer>
-        <p>version <span className="data">0.22</span> (beta): raid generation fixes</p>
+        <p>version <span className="data">1.0</span>: sharing is caring</p>
       </footer>
     );
   }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,23 +1,28 @@
 import StatCounter from "./StatCounter";
 import Hand from "./Hand";
+import ShareLinks from "./ShareLinks";
 
 function Results({ svgs, people, ants, eggs, food, eggPointValue, foodPointValue, cardsOnHand }) {
     // When the results are announced in the aria-live region, the elements are read without inserting pauses between paragraphs/headings
     // This const is to insert a visually-hidden colon that will make the screen reader pause before reading the next element
     const screenReaderPause = <span aria-label="."></span>;
 
+    const points = ants + Math.floor(eggs/eggPointValue) + Math.floor(food/foodPointValue);
+    const secretPoints = people;
+
     return (
         <>
             <section className="results stats" aria-labelledby="results">
                 <h2 id="results">Results{screenReaderPause}</h2>
                 <div>
-                    <div>
-                        <h3>Scoring{screenReaderPause}</h3>
+                    <details>
+                        <summary>View scoring information{screenReaderPause}</summary>
                         {people ? <p><span className="data">1</span> {svgs.people} person is worth <span className="data">1</span> {svgs.secretPoints} secret point.</p> : ''}
                         <p><span className="data">1</span> {svgs.ants} ant is worth <span className="data">1</span> {svgs.points} point.</p>
                         <p><span className="data">{eggPointValue}</span> {svgs.eggs} eggs are worth <span className="data">1</span> {svgs.points} point.</p>
                         <p><span className="data">{foodPointValue}</span> {svgs.food} food is worth <span className="data">1</span> {svgs.points} point.</p>
-                    </div>
+                    </details>
+
                     <div>
                         <h3>Items{screenReaderPause}</h3>
                         {people ?
@@ -28,14 +33,22 @@ function Results({ svgs, people, ants, eggs, food, eggPointValue, foodPointValue
                         <p><StatCounter stat='eggs' icon={svgs.eggs} amount={eggs} />{screenReaderPause}</p>
                         <p><StatCounter stat='food' icon={svgs.food} amount={food} />{screenReaderPause}</p>
                     </div>
+
                     <div>
                         <h3>Points{screenReaderPause}</h3>
-                        {people ? <p><span className='secret-points'>{svgs.secretPoints} {people} {svgs.secretPoints}</span>{screenReaderPause}</p> : ''}
-                        <p><span className='points'>{svgs.points} {ants + Math.floor(eggs/eggPointValue) + Math.floor(food/foodPointValue)}{screenReaderPause} {svgs.points}</span></p>
+                        {people ? <p><span className='secret-points'>{svgs.secretPoints} {secretPoints} {svgs.secretPoints}</span>{screenReaderPause}</p> : ''}
+                        <p><span className='points'>{svgs.points} {points} {screenReaderPause} {svgs.points}</span></p>
                     </div>
+
+                    <ShareLinks
+                        ants={ants}
+                        eggs={eggs}
+                        food={Math.round(food * Math.pow(10, 1 || 0)) / Math.pow(10, 1 || 0)}
+                        points={points}
+                        secretPoints={secretPoints}/>
                 </div>
-                
             </section>
+
             <Hand svgs={svgs} cards={cardsOnHand}/>
         </>
     );
